ad24389d249dd0323dfe0a2a2a37e70764ded006
XikiroRavenfang <mattsmart98@gmail.com>
2017-08-10 16:06:28 +1000

    Added beginning of Break Out code.

---
+++ Assets/4-Loops&Arrays/Scripts/Loops.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoopsArrays
+{
+    public class Loops : MonoBehaviour
+    {
+        public GameObject[] spawnPrefabs;
+        public float amplitude = 6f;
+        public float frequency = 5f;
+        public float spawnRadius = 5f;
+        public string message = "Print This";
+        public float printTime = 2f;
+        public int spawnAmount = 10;
+
+        private float timer;
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            /*
+            While (Condition)
+            {
+                Statement
+            }
+            */
+            SpawnObjectsWithSine();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+            timer += Time.deltaTime;
+            while (true)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    break;
+                }
+            }
+            */
+        }
+
+        void SpawnObjectsWithSine()
+        {
+        for (int i = 0; i < spawnAmount; i++)
+            {
+                // Spawned new game object
+                int randomIndex = Random.Range(0, spawnPrefabs.Length);
+                GameObject randomPrefab = spawnPrefabs[randomIndex];
+                GameObject clone = Instantiate(randomPrefab);
+                // Grab the MeshRenderer
+                MeshRenderer rend = clone.GetComponent<MeshRenderer>();
+                // Change the color
+                float r = Random.Range(0, 2);
+                float g = Random.Range(0, 2);
+                float b = Random.Range(0, 2);
+                rend.material.color = new Color(r, g, b);
+                // Generated random position within circle
+                float x = Mathf.Sin(i * frequency) * amplitude;
+                float y = i;
+                float z = Mathf.Cos(i * frequency) * amplitude;
+                Vector3 randomPos = transform.position + new Vector3(x, y, z);
+                // Set spawned objects positions
+                clone.transform.position = randomPos;
+            }
+        }
+
+        void SpawnObjects()
+        {
+        /*
+            for (initialization; condition; iteration0
+            {
+                statement
+            }
+        
+        for (int i = 0; i < spawnAmount; i++)
+            {
+                // Spawned new game object
+                GameObject clone = Instantiate(spawnPrefab);
+                // Generated random position within circle
+                Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius;
+                // Cancel out the z
+                randomPos.z = 0;
+                // Set spawned objects positions
+                clone.transform.position = randomPos;
+            }
+        */
+        }
+    }
+}
---
+++ Assets/~Breakout/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BreakOut
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int width = 20;
+        public int height = 20;
+        public GameObject[] blockPrefabs;
+
+        // Use this for initialization
+        void Start()
+        {
+            GenerateBlocks();
+        }
+
+        GameObject GetRandomBlock()
+        {
+            // Randomly spawn a new GameObject
+            int randomIndex = Random.Range(0, blockPrefabs.Length);
+            GameObject randomPrefab = blockPrefabs[randomIndex];
+            GameObject clone = Instantiate(randomPrefab);
+            // ... and return it
+            return clone;
+        }
+
+        void GenerateBlocks()
+        {
+            // Loop through the width
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // Create a new instance of the block
+                    GameObject block = GetRandomBlock();
+                    // Set the new position
+                    Vector3 pos = new Vector3(x, y, 0);
+                    block.transform.position = pos;
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
