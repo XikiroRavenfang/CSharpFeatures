7f5ebb42853c89454dc0b1476d367f23767a60e3
XikiroRavenfang <mattsmart98@gmail.com>
2017-08-24 12:06:47 +1000

    Added Proper Billiards Project.

--- Assets/~Billiards/Scripts/Ball.cs
+++ Assets/~Billiards/Scripts/Ball.cs
@@ -7,7 +7,7 @@ namespace Billiards
     [RequireComponent(typeof(Rigidbody))]
     public class Ball : MonoBehaviour
     {
-        public float gravity = -9.81f;
+        public float stopSpeed = 0.2f;
 
         private Rigidbody rigid;
 
@@ -18,9 +18,32 @@ namespace Billiards
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-            rigid.velocity = rigid.velocity.normalized + Vector3.back * gravity;
+            Vector3 vel = rigid.velocity;
+
+            // Check if velocity is going up
+            if(vel.y > 0)
+            {
+                // Cap velocity
+                vel.y = 0;
+            }
+
+            // If the velocity's speed is less than the stop speed
+            if(vel.magnitude < stopSpeed)
+            {
+                // Cancel out velocity
+                vel = Vector3.zero;
+            }
+
+            // Apply desired 'vel' to rigid's velocity
+            rigid.velocity = vel;
+        }
+
+        // Perform physics impact
+        public void Hit(Vector3 direction, float impactForce)
+        {
+            rigid.AddForce(direction * impactForce, ForceMode.Impulse);
         }
     }
 }
---
+++ Assets/~Billiards/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int score = 0;
+    private AudioSource sound;
+
+    // Use this for initialization
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision ball)
+    {
+        if(ball.gameObject.tag == "Ball")
+        {
+            sound.Play();
+            score++;
+            Destroy(ball.gameObject);
+        }
+    }
+}
