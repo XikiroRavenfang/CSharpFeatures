2688fbfa4fe782ab468df66a696d5b9f83f532d2
XikiroRavenfang <mattsmart98@gmail.com>
2017-08-29 15:59:33 +1000

    Added Minesweeper2D project and finished Breakout homework.

---
+++ Assets/~Minesweeper2D/Scripts/Grid.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10;
+        public int height = 10;
+        public float spacing = .155f;
+        public float offset = 0.5f;
+
+        private Tile[,] tiles;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Generate tiles on startup
+            GenerateTiles();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (true)
+            {
+                
+            }
+        }
+
+        // Functionality for spawning tiles
+        Tile SpawnTile(Vector3 pos)
+        {
+            // Clone tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos; // Position tile
+            Tile currentTile = clone.GetComponent<Tile>(); // Get tile component
+            return currentTile; // Return it
+        }
+
+        // Spawns tiles in a grid-like pattern
+        void GenerateTiles()
+        {
+            // Create new 2D array of size width x height
+            tiles = new Tile[width, height];
+
+            // Loop through the entire tile list
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // Store half size for later use
+                    Vector2 halfSize = new Vector2(width / 2, height / 2);
+                    // Pivot tiles around Grid
+                    Vector2 pos = new Vector2(x - halfSize.x + offset, y - halfSize.y + offset);
+                    // Apply spacing
+                    pos *= spacing;
+                    // Spawn the tile
+                    Tile tile = SpawnTile(pos);
+                    // Attach newly spawned tile to
+                    tile.transform.SetParent(transform);
+                    // Store it's array coordinates within itself for future reference
+                    tile.x = x;
+                    tile.y = y;
+                    // Store tile in array at those coordinates
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+
+        // Count adjacent mines at element
+        public int GetAdjacentMineCountAt(Tile t)
+        {
+            int count = 0;
+            // Loop through all elements  and have each axis go between -1 to 1
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    // Calculate desired coordinates from ones attained
+                    int desiredX = t.x + x;
+                    int desiredY = t.y + y;
+
+                    // If desired coordinates is within range of tile array length
+                    if (x >= width && y >= height)
+                    {
+                        // If the element at index is a mine
+                        if (tiles[x, y].isMine)
+                        {
+                            count++;
+                        }
+
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
