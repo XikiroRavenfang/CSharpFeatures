1f322751700a730ffd19a141faf95ae041d48f76
XikiroRavenfang <mattsmart98@gmail.com>
2017-09-21 12:15:36 +1000

    Started Inheritance Topic.

--- Assets/!Globals/Scripts/CameraOrbitWithPanAndZoom.cs
+++ Assets/!Globals/Scripts/CameraOrbitWithPanAndZoom.cs
@@ -27,7 +27,7 @@ public class CameraOrbitWithPanAndZoom : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        target.transform.SetParent(null);
+        // target.transform.SetParent(null);
 
         distance = Vector3.Distance(target.transform.position, transform.position);
 
